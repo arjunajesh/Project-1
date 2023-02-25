1f33d3596448112f1d677a0f6f4d5083bfcd27d3
arjunajesh <34989598+arjunajesh@users.noreply.github.com>
2023-02-25 01:09:36 -0500

    push

--- src/project1/International.java
+++ src/project1/International.java
@@ -1,6 +1,6 @@
 package project1;
 
-public class International extends NonResident{
+public class International extends NonResident{ //test
     private boolean isStudyAbroad;
 
     public International(String fname, String lname, Date dob, Major major, int credits, boolean isStudyAbroad) {
