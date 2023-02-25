5b5f9779f938bebc2f457a0736b593236c382755
arjunajesh <34989598+arjunajesh@users.noreply.github.com>
2023-02-25 00:39:20 -0500

    added sub classes of student implemented all add student commands

---
+++ src/project1/International.java
@@ -0,0 +1,10 @@
+package project1;
+
+public class International extends NonResident{
+    private boolean isStudyAbroad;
+
+    public International(String fname, String lname, Date dob, Major major, int credits, boolean isStudyAbroad) {
+        super(fname, lname, dob, major, credits);
+        this.isStudyAbroad = isStudyAbroad;
+    }
+}
---
+++ src/project1/Resident.java
@@ -0,0 +1,9 @@
+package project1;
+
+public class Resident extends Student{
+
+    private int scholarship;
+    public Resident(String fname, String lname, Date dob, Major major, int credits) {
+        super(fname, lname, dob, major, credits);
+    }
+}
