6800f9b868943a6183d82df806f063d1d36104b9
arjunajesh <34989598+arjunajesh@users.noreply.github.com>
2023-02-27 02:15:26 -0500

    CT bug fix

--- src/project1/TuitionManager.java
+++ src/project1/TuitionManager.java
@@ -180,7 +180,7 @@ public class TuitionManager {
                         System.out.println("Missing the state code.");
                         return false;
                     }
-                    if(!command[6].equalsIgnoreCase("NY") || !command[6].equalsIgnoreCase("NY")){
+                    if(!command[6].equalsIgnoreCase("NY") || !command[6].equalsIgnoreCase("CT")){
                         System.out.println(command[6] + ": Invalid state code.");
                         return false;
                     }
